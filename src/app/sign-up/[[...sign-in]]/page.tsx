4b621ca0edfff103624f1a48df47bab3217e5acd
sahil <leviack629@gmail.com>
2025-02-04 00:29:35 +0530

    fix signup page steps ui

--- src/app/sign-up/[[...sign-in]]/page.tsx
+++ src/app/sign-up/[[...sign-in]]/page.tsx
@@ -63,7 +63,7 @@ export default function SignUpPage() {
         </SignUp.Step>
         <SignUp.Step
           name="verifications"
-          className="w-full space-y-6 rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8"
+          className="w-full space-y-6 rounded-2xl  px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8"
         >
           <header className="text-center">
             <svg
@@ -88,28 +88,28 @@ export default function SignUpPage() {
                 <path d="M27 3.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 8.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM23 13.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM21.5 18.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM20.5 23.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM22.5 28.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 33.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM27 38.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2Z" />
               </g>
             </svg>
-            <h1 className="mt-4 text-xl font-medium tracking-tight text-zinc-950">
+            <h1 className="mt-4 text-xl font-medium tracking-tight ">
               Verify email code
             </h1>
           </header>
           <Clerk.GlobalError className="block text-sm text-red-400" />
           <SignUp.Strategy name="email_code">
             <Clerk.Field name="code" className="space-y-2">
-              <Clerk.Label className="text-sm font-medium text-zinc-950">
+              <Clerk.Label className="text-sm font-medium ">
                 Email code
               </Clerk.Label>
               <Clerk.Input
                 type="otp"
                 required
-                className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+                className="w-full rounded-md  px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
               />
               <Clerk.FieldError className="block text-sm text-red-400" />
             </Clerk.Field>
             <SignUp.Action
               submit
-              className="w-full rounded-md bg-zinc-950 px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
+              asChild
             >
-              Verify
+              <Button variant={"outline"}>Verify</Button>
             </SignUp.Action>
           </SignUp.Strategy>
           <p className="text-center text-sm text-zinc-500">
@@ -124,7 +124,7 @@ export default function SignUpPage() {
         </SignUp.Step>
         <SignUp.Step
           name="continue"
-          className="w-full space-y-6 rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8"
+          className="w-full space-y-6 rounded-2xl  px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8"
         >
           <header className="text-center">
             <svg
@@ -149,25 +149,25 @@ export default function SignUpPage() {
                 <path d="M27 3.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 8.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM23 13.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM21.5 18.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM20.5 23.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM22.5 28.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 33.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM27 38.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2Z" />
               </g>
             </svg>
-            <h1 className="mt-4 text-xl font-medium tracking-tight text-zinc-950">
+            <h1 className="mt-4 text-xl font-medium tracking-tight ">
               Continue registration
             </h1>
           </header>
           <Clerk.GlobalError className="block text-sm text-red-400" />
           <Clerk.Field name="username" className="space-y-2">
-            <Clerk.Label className="text-sm font-medium text-zinc-950">
+            <Clerk.Label className="text-sm font-medium ">
               Username
             </Clerk.Label>
             <Clerk.Input
               type="text"
               required
-              className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+              className="w-full rounded-md px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
             />
             <Clerk.FieldError className="block text-sm text-red-400" />
           </Clerk.Field>
           <SignUp.Action
             submit
-            className="w-full rounded-md bg-zinc-950 px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
+            className="w-full rounded-md bg-zinc-950 px-3.5 py-1.5 text-center text-sm font-medium shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 "
           >
             Continue
           </SignUp.Action>
